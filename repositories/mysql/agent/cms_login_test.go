88ded9e88af769fe2d93b7c815bf93ec35c30772
Mohammad Julfikar <jxmxshakil@gmail.com>
2024-07-12 14:46:02 +0800

    - test added

--- repositories/mysql/agent/cms_login_test.go
+++ repositories/mysql/agent/cms_login_test.go
@@ -14,15 +14,16 @@ func TestResolve(t *testing.T) {
 		return
 	}
 	repo := NewCmsLoginRepository(db)
-	_, err = repo.Get(0)
+	res, err := repo.GetAll()
 	if err != nil {
 		t.Error(err)
 		t.Fail()
 		return
 	}
+	t.Logf("TestResolve:GetAll %v", len(res))
 }
 
 func dbConn() (db *xorm.Engine, err error) {
-	conn := fmt.Sprintf("root:mysql@tcp(127.0.0.1:3306)/easysale_elk?charset=utf8mb4&parseTime=True&loc=Local&timeout=2s")
+	conn := fmt.Sprintf("root:mysql@tcp(127.0.0.1:3306)/easysale_vit?charset=utf8mb4&parseTime=True&loc=Local&timeout=2s")
 	return xorm.NewEngine("mysql", conn)
 }
